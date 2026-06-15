e33c533aa9f1c45f6904b9f77b5ebede54d48d61
sea2horses <thefuzegamer06@gmail.com>
2026-06-15 02:20:26 -0600

    feat: crear proyecto de ktor

---
+++ backend/build.gradle.kts
@@ -0,0 +1,39 @@
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(ktorLibs.plugins.ktor)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+group = "ni.uam.edu"
+version = "1.0.0-SNAPSHOT"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+dependencies {
+    implementation(ktorLibs.serialization.kotlinx.json)
+    implementation(ktorLibs.server.auth)
+    implementation(ktorLibs.server.config.yaml)
+    implementation(ktorLibs.server.contentNegotiation)
+    implementation(ktorLibs.server.core)
+    implementation(ktorLibs.server.netty)
+    implementation(ktorLibs.server.openapi)
+    implementation(ktorLibs.server.routingOpenapi)
+    implementation(ktorLibs.server.statusPages)
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.r2dbc)
+    implementation(libs.h2database.h2)
+    implementation(libs.h2database.r2dbc)
+    implementation(libs.kborowy.firebaseAuthProvider)
+    implementation(libs.logback.classic)
+    implementation(libs.postgresql)
+    implementation(libs.r2dbc.postgresql)
+
+    testImplementation(kotlin("test"))
+    testImplementation(ktorLibs.server.testHost)
+}
